4bd7c39ab14e110df86e7c747cc5c918e8389550
RuaAbbadi <abbadirua@gmail.com>
2026-01-16 17:34:45 +0200

    correct import of CustomAPIError from index

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,5 @@
 const { StatusCodes } = require("http-status-codes");
-const CustomAPIError = require("../errors/index");
+const {CustomAPIError} = require("../errors/index");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
