de53de1554f937b233d4fb17eeb3d2721def73c8
RuaAbbadi <abbadirua@gmail.com>
2026-01-16 16:19:31 +0200

    Add authentication controllers and routes

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Jobs API");
-});
+
+//routers
+const authRouter = require("./routes/auth");
+
+app.use("/api/v1/auth", authRouter);
+
 
 app.listen(3000, () => console.log("server is listening on port 3000"));
