b02dd23d20963207c2e57526b6813ae5dd6e352e
RuaAbbadi <abbadirua@gmail.com>
2026-01-16 16:31:46 +0200

    Configure database connection and env setup

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
-
+//connectDB
+const connectDB = require("./db/connect");
 //routers
 const authRouter = require("./routes/auth");
 
 app.use("/api/v1/auth", authRouter);
 
+const port = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log("server is listening on port 3000"));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => console.log(`server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
